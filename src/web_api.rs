2f993f84ba25b236603c64217d4c133e7d749913
Masaru Ueki <masaru.ueki@itage.co.jp>
2017-06-04 00:09:36 +0900

    Tokenを使ってWebAPI呼び出し

--- src/web_api.rs
+++ src/web_api.rs
@@ -1,10 +1,15 @@
 
 const API_URL: &'static str = "https://slack.com/api/";
 
+use std::io::Read;
+
+use hyper::Url;
 use hyper::client::*;
 use hyper::Client;
 use hyper::net::HttpsConnector;
 use hyper_native_tls::NativeTlsClient;
+
+use std::env;
 
 pub fn get(method: &str) -> String {
 
@@ -12,10 +17,17 @@ pub fn get(method: &str) -> String {
     let connector = HttpsConnector::new(tls);
     let client = Client::with_connector(connector);
 
-    let url = API_URL.to_string() + method;
-    let res = client.get(&url).send().unwrap();
+    let mut url = Url::parse(&(API_URL.to_string() + method)).unwrap();
 
-    println!("res = {:?}", res);
+    let token = env::var("SLACKBOT_TOKEN").unwrap();
+    url.query_pairs_mut().append_pair("token", &token);
 
-    return url;
+    let mut res = client.get(url).send().unwrap();
+
+    let mut buf =  String::new();
+    res.read_to_string(&mut buf).unwrap();
+
+    println!("res = {:?}", buf);
+
+    return "hoge".to_string();
 }
